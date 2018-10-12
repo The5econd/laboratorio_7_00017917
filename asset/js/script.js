3276ddb600345e708e0e80ea7bdcd1a8d6991ffe
Fernando Salazar <00017917@uca.edu.sv>
2018-10-12 10:21:56 -0600

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,31 @@
+
+window.addEventListener("load",()=>{
+    var container;
+    container = document.getElementById("c1");
+    container.innerText = "¡Hola mundo!";
+    container.style.background= "#000000";
+    container.style.color = "#1cb723";
+    container.style.width = "200px";
+    container.style.height = "200px";
+    container.innerHTML =  "<input type='text' placeholder='escriba su texto'>";
+
+
+    var containers = document.querySelectorAll("div.c2");
+    containers.forEach(element => {
+        element.style.background =  "#009999";
+    });
+    containers.innerHTML =  "<input type='text' placeholder='escriba su texto'>";
+
+
+    boton = document.getElementById("btn-click");
+    boton.onclick = function(evt){
+        alert("Hola mundo");
+    }
+    btncopy = document.getElementById("btncopy");
+    btnCopy.onclick = function(evt){
+        let msj = document.getElementById('textMsj').value;
+        document.getElementById('showMsj').innerText = msj;
+    } 
+});
+
+
